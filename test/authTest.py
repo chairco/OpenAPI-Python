8fc30dfc4b4a87508157ad299f00560dcad51f8c
chairco <chairco@gmail.com>
2017-12-19 14:59:20 +0800

    Adjust read id and secret for each test case

--- test/authTest.py
+++ test/authTest.py
@@ -2,11 +2,11 @@
 # encoding: utf-8
 
 import unittest
-import sys
-from os import path
-sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
+
 from kkbox_developer_sdk.auth_flow import *
-from client import ClientInfo
+
+from env import ClientInfo
+
 
 CLIENT_ID = ClientInfo.client_id
 CLIENT_SECRET = ClientInfo.client_secret
@@ -14,13 +14,13 @@ CLIENT_SECRET = ClientInfo.client_secret
 
 class TestAuthSDK(unittest.TestCase):
 
-	def test_fetch_access_token_by_client_credentials(self):
-		auth = KKBOXOAuth(CLIENT_ID, CLIENT_SECRET)
-		auth.fetch_access_token_by_client_credentials()
-		access_token = auth.access_token
-		assert access_token.access_token != None
-		assert access_token.expires_in != None
-		assert access_token.token_type != None
+    def test_fetch_access_token_by_client_credentials(self):
+        auth = KKBOXOAuth(CLIENT_ID, CLIENT_SECRET)
+        auth.fetch_access_token_by_client_credentials()
+        access_token = auth.access_token
+        assert access_token.access_token != None
+        assert access_token.expires_in != None
+        assert access_token.token_type != None
 
 if __name__ == '__main__':
     unittest.main()
